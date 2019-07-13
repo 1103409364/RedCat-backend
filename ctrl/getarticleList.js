f3492a07a0b197cce233633c7a6085444eb7b8c0
1103409364 <1103409364@qq.com>
2019-07-13 19:35:08 +0800

    文章列表接口分页加入分页

--- ctrl/getarticleList.js
+++ ctrl/getarticleList.js
@@ -1,12 +1,22 @@
 const Article = require('../models/Article');
 
 module.exports = (req, res) => {
-    // find 不传参数就是查找所有
-    Article.find()
-        .then(articles => {
-            const data = {success: true, data: articles}
-            // console.log(articles)
-            res.json(data);
+    // 每页的条数
+    const onePage = 4;
+    const page = req.query.page;
+    Article.countDocuments({})
+        .then(result => {
+            // 计算总页数
+            let totalPage = Math.ceil(result / onePage);
+            // find 查找所有，这里可以使用 sort 方法排序 1、asc为升序，-1、desc为降序。分页：跳过几页读几行
+            Article.find({}).limit(onePage).skip(onePage * (page - 1))
+                .then(articles => {
+                    // 带上总页数
+                    const data = { success: true, totalPage, data: articles }
+                    res.json(data);
+                })
+                .catch(err => console.log(err));
         })
-    
+
+
 }
