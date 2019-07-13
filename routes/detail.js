c64ebfade2d9ac5351300699aae3d6dbdb4c2b2f
1103409364 <1103409364@qq.com>
2019-07-13 15:08:23 +0800

    新增详情页接口

--- routes/detail.js
+++ routes/detail.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const getArticleDetail = require('../ctrl/getArticleDetail');
-
+// 详情页接口
 router.get('/article', getArticleDetail);
 
 module.exports = router;
