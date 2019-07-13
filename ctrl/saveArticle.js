26873d64f07b28f2489df8ea872ad6c4916ab90b
1103409364 <1103409364@qq.com>
2019-07-13 20:17:56 +0800

    文章保存接口新增：拦截 Email 未激活的用户

--- ctrl/saveArticle.js
+++ ctrl/saveArticle.js
@@ -1,18 +1,35 @@
 const Article = require('../models/Article');
+const User = require('../models/User');
 
 module.exports = (req, res) => {
     const article = req.body;
     const data = {};
-
-    Article.saveToDB(article)
-        .then((result) => {
-            // result 是新建的 article 对象
-            data.success = true;
-            data.result = result;
-            res.json(data);
-        }, err => {
+    const author = article.author;
+    
+    // 拦截未激活用户，验证用户是否激活，用户信息应该从 token 中读取？
+    User.findOne({ name: author }).then(user => {
+        if (user.name === author) {
+            // console.log(user.name);
+            Article.saveToDB(article)
+                .then((result) => {
+                    // result 是新建的 article 对象
+                    data.success = true;
+                    data.result = result;
+                    res.json(data);
+                }, err => {
+                    data.success = false;
+                    data.err = err;
+                    res.json(data);
+                }).catch(err => {
+                    close.log(err);
+                });
+        } else {
             data.success = false;
-            data.err = err;
             res.json(data);
-        });
+        }
+    }).catch(err =>{
+        close.log(err);
+    })
+
+
 }
